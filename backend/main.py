53ab8c86ccdb2abce225a24f297dc246b589e332
Jordi saleilles <jordisaleilles@hotmail.fr>
2023-01-24 20:35:17 -0500

    Added dialogflow detect intent function.

---
+++ backend/main.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+from src import dialogflow
+import os
+
+load_dotenv()
+
+DIALOGFLOW_PROJECT_ID = os.environ["DIALOGFLOW_PROJECT_ID"]
+DIALOGFLOW_SESSION_ID = os.environ["DIALOGFLOW_SESSION_ID"]
+DIALOGFLOW_LANGUAGE_CODE = os.environ["DIALOGFLOW_LANGUAGE_CODE"]
+
+def main():
+    texts = "Hello"
+    print(dialogflow.detect_intent_texts(DIALOGFLOW_PROJECT_ID, DIALOGFLOW_SESSION_ID, texts, DIALOGFLOW_LANGUAGE_CODE))
+
+main()
