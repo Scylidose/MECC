1cada46b4daa74a3f0ef06169dbef31e9702942f
Jordi saleilles <jordisaleilles@hotmail.fr>
2023-01-29 19:28:39 -0500

    Removed discord api.

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,9 @@
 from dotenv import load_dotenv
 from src import dialogflow_mecc
-from src.discord import discord_mecc
 
 import os
 
 load_dotenv()
-
-DISCORD_BOT_TOKEN = os.environ["DISCORD_BOT_TOKEN"]
 
 DIALOGFLOW_PROJECT_ID = os.environ["DIALOGFLOW_PROJECT_ID"]
 DIALOGFLOW_SESSION_ID = os.environ["DIALOGFLOW_SESSION_ID"]
@@ -16,9 +13,6 @@ def main():
 
     texts = "Hello"
     dialogflow_client = dialogflow_mecc.DialogflowMECC(DIALOGFLOW_PROJECT_ID, DIALOGFLOW_SESSION_ID, DIALOGFLOW_LANGUAGE_CODE)
-    
-    discord_client = discord_mecc.DiscordMECC(dialogflow_client, DISCORD_BOT_TOKEN)
-    discord_client.run_discord_bot()
 
     fullfilment_text = dialogflow_client.detect_intent_texts(texts)
 
