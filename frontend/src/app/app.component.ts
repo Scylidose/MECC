689d1ae6e0626dc5f8357ef58c83366254b5f69b
Jordi saleilles <jordisaleilles@hotmail.fr>
2023-02-02 17:49:47 -0500

    Added more styled angular interface and communication with Flask.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,68 @@
-import {Component} from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import {ChatbotApiService} from "./chatbot/chatbot-api.service";
+import {Router} from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  template: `
-    <div style="text-align:center">
-      <h1>MECC</h1>
-      <h4>Miscellaneous Educational Cybersecurity Chatbot</h4>
-    </div>
-    <router-outlet></router-outlet>
-  `,
-  styleUrls: ['./app.component.less']
+ selector: 'app-root',
+ templateUrl: './app.component.html',
+ styleUrls: ['./app.component.less']
 })
-export class AppComponent { }
+export class AppComponent {
+ title = 'chat-ui';
+
+ message = "";
+
+ chatbot = {
+     messages: "",
+ };
+
+ constructor(private chatbotApi: ChatbotApiService, private router: Router) { }
+
+ @ViewChild('chatListContainer') list?: ElementRef<HTMLDivElement>;
+ chatInputMessage: string = "";
+ 
+ chatMessages: {
+  message: string
+}[] = [
+  {
+    message: "Hello I'm MECC or Miscellaneous Educational Cybersecurity Chatbot\n"
+  },
+  {
+    message: "A chatbot specialized in cybersecurity education and sensibilization."
+  }
+];
+
+ ngAfterViewChecked() {
+   this.scrollToBottom()
+ }
+
+
+ saveChatbot() {
+  this.chatbot.messages = this.message;
+  this.chatMessages.push({
+    message:  this.message
+  });
+  this.chatbotApi.send(this.chatbot).subscribe(data => {
+    this.receive(data.messages[data.messages.length - 1]);
+  });
+  this.chatbotApi
+    .saveChatbot(this.chatbot)
+    .subscribe(
+      () => this.router.navigate(['/']),
+      error => alert(error.message)
+    );
+  this.chatInputMessage = ""
+  this.scrollToBottom()
+}
+
+receive(message: string) {
+  this.chatMessages.push({
+    message: message
+  });
+this.scrollToBottom()
+}
+ scrollToBottom() {
+   const maxScroll = this.list?.nativeElement.scrollHeight;
+   this.list?.nativeElement.scrollTo({top: maxScroll, behavior: 'smooth'});
+ }
+}
