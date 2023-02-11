92f9cc2b65847b73a0190d880b23d4490f6485ca
Lipode <jordisaleilles@hotmail.fr>
2023-02-11 15:17:19 -0500

    Gets quiz results and display them on the left panel.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -34,6 +34,16 @@ export class AppComponent {
     id: 2
   }
 
+  chatQuizResults: {
+    quiz_results: Array<string>,
+    score: any,
+    topic_list: Array<string>
+  } = {
+    quiz_results: [],
+    score: {},
+    topic_list: []
+  }
+
   chatYoutube: {
     message: string
   }[] = []
@@ -101,6 +111,7 @@ export class AppComponent {
       error => alert(error.message)
     );
   this.chatInputMessage = ""
+
   this.scrollToBottom()
 }
 
@@ -121,9 +132,18 @@ replaceInput(quick_reply: string) {
       () => this.router.navigate(['/']),
       error => alert(error.message)
     );
-  this.chatInputMessage = ""
+  this.chatInputMessage = "";
 
-  this.scrollToBottom()
+  this.scrollToBottom();
+
+  if(quick_reply == "Humans" || 
+     quick_reply == "A weak encryption" ||
+     quick_reply == "Short passwords"){
+    this.chatbotApi.getResults().subscribe(data => {
+      this.chatQuizResults = data
+    });
+  }
+
  };
 
 
